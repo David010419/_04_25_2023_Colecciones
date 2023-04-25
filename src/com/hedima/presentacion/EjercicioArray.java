6800b29707983c0c2dd3feacdcb45d471be91604
David010419 <dcastillo@global-retail.com>
2023-04-25 21:20:07 +0200

    25-04-2023

--- src/com/hedima/presentacion/EjercicioArray.java
+++ src/com/hedima/presentacion/EjercicioArray.java
@@ -1,2 +1,34 @@
-package com.hedima.presentacion;public class EjercicioArray {
+package com.hedima.presentacion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EjercicioArray {
+    public static void main(String[] args) {
+
+        int[] a = {1,2,3,4,5,6,7,8,9};
+
+            System.out.println("Array" + Arrays.toString(a));
+
+            List<Integer> a1 = new ArrayList<>();
+
+            for (int i = 1; i < 10; i++) {
+                a1.add(i);
+            }
+
+        System.out.println("List " +a1);
+//Ejercicio Claudio
+//        int[] a1= new int[10];
+//        for (int i = 0; i <10;i++){
+//            a1[i] =i;
+//        }
+//        List lista=new ArrayList();
+//        for (int j = 0; j <10;j++){
+//            lista.add(j);
+//        }
+//        System.out.println(Arrays.toString(a1));
+//        System.out.println(lista);
+
+    }
 }
--- src/com/hedima/presentacion/Main.java
+++ src/com/hedima/presentacion/Main.java
@@ -1,5 +1,35 @@
+package com.hedima.presentacion;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Declaracion de variables
+        Scanner s1 = new Scanner(System.in);
+
+        String[] palabras;
+
+        Set<String> diferentes= new HashSet<>();
+
+        String texto;
+        //Solicitar frase
+        System.out.println("Escriba un texto para analizar -> ");
+        texto = s1.nextLine();
+
+        palabras = texto.split(" ");
+        for (String elemento : palabras) {
+            if (elemento.trim().length()>0){
+                diferentes.add(elemento.trim().toLowerCase());
+            }
+        }
+        System.out.println("Palabras diferentes -> ");
+        for (String elemento : diferentes) {
+            System.out.print(elemento +",");
+        }
+        System.out.println("¿Cuantas palabras diferentes hay?" + diferentes.size());
+        System.out.println("¿Cuantas palabras tiene la frase?" + palabras.length);
     }
 }
